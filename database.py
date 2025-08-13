ed4fd00dbb7498294b280c70b7ba4f5493f6dcac
leegyumin <leebadagyumin0129@gmail.com>
2025-08-13 20:57:09 +0900

    feat : db 연결 개발

---
+++ database.py
@@ -0,0 +1,36 @@
+import pymysql
+from contextlib import contextmanager
+import os
+
+# from dotenv import load_dotenv
+# load_dotenv("./.env")
+
+DB_CONFIG = {
+    "host": os.getenv("DB_ADRESS"),
+    "port": int(os.getenv("DB_PORT")),
+    "user": "root",
+    "password": os.getenv("DB_PASSWORD"),
+    "db": "velog",
+    "charset": "utf8mb4",
+    "autocommit": False
+}
+
+@contextmanager
+def get_connection():
+    conn = pymysql.connect(**DB_CONFIG)
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+def get_db():
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        try:
+            yield cursor
+            conn.commit()
+        except Exception:
+            conn.rollback()
+            raise
+        finally:
+            cursor.close()
--- routers/posts.py
+++ routers/posts.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from database import get_db
 
 router = APIRouter(prefix="/posts", tags=["posts"])
 
 @router.get("/")
-def read_post_list(bunch: int = 0):
-    return {"message": "hello fastapi", "bunch": bunch}
+def read_post_list(bunch: int = 0, db=Depends(get_db())):
+    db.execute("select * from velog.test;")
+    return {"message": db.fetchall(), "bunch": bunch}
 
