619505e5aee5b5c66b0563220c014028242d16ff
leegyumin <leebadagyumin0129@gmail.com>
2025-08-13 20:04:48 +0900

    chore : cloudtype test용

--- routers/posts.py
+++ routers/posts.py
@@ -4,5 +4,5 @@ router = APIRouter(prefix="/posts", tags=["posts"])
 
 @router.get("/")
 def read_post_list(bunch: int = 0):
-    return {"message": "hello world", "bunch": bunch}
+    return {"message": "hello fastapi", "bunch": bunch}
 
