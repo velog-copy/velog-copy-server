fd516fd98aea5175a557a2cbbc002569e5ce0783
leegyumin <leebadagyumin0129@gmail.com>
2025-08-13 21:53:00 +0900

    fix : get_db 함수 객체를 의존하게 변경

--- routers/posts.py
+++ routers/posts.py
@@ -4,7 +4,7 @@ from database import get_db
 router = APIRouter(prefix="/posts", tags=["posts"])
 
 @router.get("/")
-def read_post_list(bunch: int = 0, db=Depends(get_db())):
+def read_post_list(bunch: int = 0, db=Depends(get_db)):
     db.execute("select * from velog.test;")
     return {"message": db.fetchall(), "bunch": bunch}
 
