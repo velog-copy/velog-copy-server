16f1baa316ebf495459c5a36ab7bc1a97fd00b1f
leegyumin <leebadagyumin0129@gmail.com>
2025-08-14 12:01:08 +0900

    feat : image 자원 삭제 기능 개발

--- routers/resources.py
+++ routers/resources.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter, Request, Response, UploadFile, File, Depends
 from fastapi.responses import StreamingResponse
-import io
 from database import get_db
-from services.resources import register_image, get_image
+from services.resources import register_image, get_image, remove_image
 from models.resources import T_image
+import io
 
 router = APIRouter(prefix="/resources", tags=["resources"])
+
+@router.post("/image")
+def create_image(file: UploadFile = File(...), db=Depends(get_db)):
+    image_id = register_image(file.file, db)
+
+    if image_id is None:
+        return Response(status_code=415)
+
+    return {"image_id": image_id}
 
 @router.get("/image/{image_id}")
 def read_image(image_id: int, request: Request, db=Depends(get_db)):
@@ -24,12 +33,11 @@ def read_image(image_id: int, request: Request, db=Depends(get_db)):
 
     return StreamingResponse(io.BytesIO(data.image_data), media_type="image/jpeg", headers=headers)
 
+@router.delete("/image/{image_id}")
+def delete_image(image_id: int, db=Depends(get_db)):
+    try:
+        remove_image(image_id, db)
+    except:
+        return Response(status_code=409)
 
-@router.post("/image")
-def create_image(file: UploadFile = File(...), db=Depends(get_db)):
-    image_id = register_image(file.file, db)
-
-    if image_id is None:
-        return Response(status_code=415)
-
-    return {"image_id": image_id}
+    return Response(status_code=200)
--- services/resources.py
+++ services/resources.py
@@ -1,9 +1,8 @@
-from fastapi import UploadFile
-import io
 from PIL import Image, UnidentifiedImageError
-from typing import BinaryIO
-import pymysql as sql
 from models.resources import T_image
+import pymysql as sql
+from typing import BinaryIO
+import io
 
 
 def get_image(image_id: int, db: sql.cursors.DictCursor) -> T_image | None:
@@ -16,20 +15,23 @@ def get_image(image_id: int, db: sql.cursors.DictCursor) -> T_image | None:
     result = T_image(**row)
     return result
 
-def register_image(file: BinaryIO, db: sql.cursors.DictCursor):
+def register_image(file: BinaryIO, db: sql.cursors.DictCursor) -> int:
     try:
         image = Image.open(file)
     except UnidentifiedImageError:
         return None
 
-    max_size = (500, 500)
+    max_size = (600, 600)
     image.thumbnail(max_size)
 
     temp_file = io.BytesIO()
-    image.convert("RGB").save(temp_file, format="JPEG", quality=75, exif=b"")
+    image.convert("RGB").save(temp_file, format="JPEG", quality=90, exif=b"")
     image_data = temp_file.getvalue()
     
     db.execute("insert into images (image_id, change_id, image_data) values (%s, %s, %s);", (None, 0, image_data))
     image_id = db.lastrowid
 
     return image_id
+
+def remove_image(image_id: int, db: sql.cursors.DictCursor) -> None:
+    db.execute("delete from images where image_id = %s", (image_id))
