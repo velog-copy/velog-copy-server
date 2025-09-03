792e469c44a8580d7f457da70c3b54d34c434d2b
가희 <leebadagyumin0129@gmail.com>
2025-09-03 17:54:54 +0900

    fix : 좋아요 계수 기능 버그 수정

--- services/likes.py
+++ services/likes.py
@@ -14,7 +14,7 @@ def get_likes(user_id: int, db: DictCursor) -> list | None:
 def insert_likes(posting_id: int, user_id: int, db: DictCursor) -> bool:
     try:
         db.execute("INSERT INTO likes VALUES (%s, %s);", (posting_id, user_id))
-        db.execute("UPDATE posting SET likes_count = likes_count + 1 WHERE posting_id = %s;", posting_id)
+        db.execute("UPDATE postings SET likes_count = likes_count + 1 WHERE posting_id = %s;", posting_id)
         return True
     except:
         return False
@@ -22,7 +22,7 @@ def insert_likes(posting_id: int, user_id: int, db: DictCursor) -> bool:
 def delete_likes(posting_id: int, user_id: int, db: DictCursor) -> bool:
     try:
         db.execute("DELETE FROM likes WHERE posting_id = %s and user_id = %s;", (posting_id, user_id))
-        db.execute("UPDATE posting SET likes_count = likes_count - 1 WHERE posting_id = %s;", posting_id)
+        db.execute("UPDATE postings SET likes_count = likes_count - 1 WHERE posting_id = %s;", posting_id)
         return True
     except:
         return False
